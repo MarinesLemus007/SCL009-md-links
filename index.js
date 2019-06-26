51427009db6418a19752ccd8baf4110042ea242a
MarinesLemus007 <marineslemus@gmail.com>
2019-06-26 08:56:31 -0400

    lee si es archivo o directorio,extrae los link del archivo y ve su status

--- index.js
+++ index.js
@@ -1,47 +1,102 @@
-// import { fstat } from "fs"; no sé
-
+#!/usr/bin/env node
 // module.exports = () => {
 //   // ...
 // };
 
+const fetch = require('node-fetch');
+const path = require('path');
+const process = require("process");
 const marked = require("marked");
-
 const fs = require('fs');
+const FileHound = require('filehound');
 
-// fs.readFile("./prueba.md", "utf8",(err, data) => {
-//   if(err){
-//     throw err
-//   }
-  
-//     console.log(data);
-  
-// })
+let terminalValue = [];
 
+console.log(process.argv);
 
-const links = (path) =>{
-  fs.readFile(path,"utf8", (err,data) =>{
-    if(err){
-      throw err;
+process.argv.forEach((val, index) => {
+  terminalValue.push(process.argv[index]);
+  //console.log(`${index}: ${val}`);
+});
+console.log(terminalValue);
+  //terminalValue[2] = path
+
+  fs.stat(terminalValue[2], (err, stats) => {
+    
+    if (err) {
+      console.error(err)
     }
-    let links =[];
+  
+    if(stats.isFile()){
 
-    const renderer = new marked.Renderer();
-
-    renderer.link = function(href, title, text){
-
-      links.push({
-        
-        href:href,
-        text:text,
-        file:path
-      
-      })
+      console.log("Soy un archivo");
+      links(terminalValue[2]);
 
     }
-    marked(data, {renderer:renderer})
-      console.log(links)
+
+    if(stats.isDirectory()){
+      console.log("Soy un directorio");
+      SearDirectoryFile(terminalValue[2]);
+    }
+    
   })
 
-}
+  const links = (path) =>{
+    fs.readFile(path,"utf8", (err,data) =>{
+      if(err){
+        throw err;
+      }
+        let links =[];
+  
+      const renderer = new marked.Renderer();
+  
+      renderer.link = function(href, title, text){
+  
+        links.push({
+          
+          href:href,
+          text:text,
+          file:path
+        
+        })
+  
+      }
+      marked(data, {renderer:renderer})
+        console.log(links);
+        fetchlinks(links);
+    })
+  
+  }
+  //links(terminalValue[2]);
 
-console.log(links("./prueba.md"));
+  const fetchlinks = (links) => {
+    let hrefLink = [];
+    hrefLink = links.map(link=>{
+    return link.href;
+    });
+
+    console.log(hrefLink);
+
+  hrefLink.forEach(element => {
+  fetch(element)
+    .then(res => {
+     
+        console.log(res.url);
+        console.log(res.ok);
+        console.log(res.status);
+        console.log(res.statusText);
+  
+    });
+        })
+        
+      }
+
+  const SearDirectoryFile = (directory) =>{
+
+  const files = FileHound.create()
+    .paths(directory)
+    .ext('md')
+    .find();
+  files.then(console.log);
+  
+}
